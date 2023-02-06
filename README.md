1d779f550d61570b12e7894abe633d541a834f19
Peterforyou <110077295+Peterforyou@users.noreply.github.com>
2023-02-06 17:45:05 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# WORD SCRAMBLE GAME
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+  - [Continued development](#continued-development)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+
+## Overview
+This is a word scramble game where users gets to enter the correct word which is being scrambled.
+
+### Screenshot
+
+![](./Annotation%202023-02-06%20173659.png)
+
+### Links
+
+- Live Site URL:(https://peterforyou.github.io/passenger-counter-app/)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- JavaScript
+
+### Continued development    
+
+I would love to focus more on React
+
+## Author
+
+- Name - [Peter Bit](https://www.twitter.com/Peterbyte2)
+
+## Acknowledgments
+
+Am aknowledging my humble self for picking up this challenge.
---
+++ script.js
@@ -0,0 +1,80 @@
+const wordEl = document.getElementById('word-scramble');
+const hintEl = document.querySelector('#hint span');
+const timeEl = document.querySelector('#time span');
+const inputEl = document.getElementById('input-word');
+const refreshEl = document.getElementById('refresh');
+const checkEl = document.getElementById('check');
+
+let word = '';
+let time = 10;
+let timer;
+
+function initGame () {
+    let randomIndex = Math.floor(Math.random() * words.length);
+    let randomObj = words[randomIndex];
+    // console.log(randomObj);
+    word = randomObj.word
+    // console.log(word)
+    let wordArry = word.split('').sort( () => Math.random() - 0.5);
+    // console.log(wordArry)
+    let scrambledWord = wordArry.join('');
+    console.log(scrambledWord)
+
+    if (scrambledWord === word) {
+        return initGame();
+    }
+
+    wordEl.innerText = scrambledWord;
+    hintEl.textContent = randomObj.hint;
+    timeEl.textContent = time;
+    inputEl.value = '';
+    checkEl.setAttribute('disabled', true)
+
+    timer = setInterval(() => {
+        if (time > 0) {
+            time--
+            return timeEl.textContent = time;
+        }
+        timeOut(`Time out! ${word.toUpperCase()} is a correct word`)
+    },1000);
+    console.log(timer)
+}
+
+initGame()
+
+refreshEl.addEventListener('click', () => {
+    timeOut();
+})
+
+function refreshGame (msg) {
+    if (msg) {
+        alert(msg)
+    }
+    time = 10;
+    clearInterval(timer);
+    initGame()
+}
+
+function timeOut (msg) {
+    refreshGame(msg);
+}
+
+checkEl.addEventListener('click', () => {
+    checkWord();
+})
+ inputEl.addEventListener ('input', (e) => {
+    if (!e.target.value) {
+        checkEl.setAttribute('disabled', true)
+    } else {
+        checkEl.removeAttribute('disabled')
+    }
+ })
+function checkWord (){
+    
+    if (inputEl.value === word) {
+        alert('Wowhh! you got it correctly!')
+        timeOut()
+    }  else {
+        alert(`${inputEl.value.toUpperCase()} is not the correct word`)
+    }
+}
